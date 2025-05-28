005dd371a67a1b1f3c3ff786cc99e65242bfc664
Zainal Saputra <101336276+zainalsaputra@users.noreply.github.com>
2025-05-29 01:20:04 +0700

    pre commit #2

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-undef': 'warn',
+      '@typescript-eslint/no-require-imports': 'warn',
+    },
+  },
+]);
