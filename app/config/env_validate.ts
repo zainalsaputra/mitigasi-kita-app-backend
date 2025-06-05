8d85973e18089a449bcd564bd543ef4f1d9db774
Zainal Saputra <101336276+zainalsaputra@users.noreply.github.com>
2025-06-05 12:08:46 +0700

    unused mongo prisma change mongoose & .ignore generated folder

--- app/config/env_validate.ts
+++ app/config/env_validate.ts
@@ -26,9 +26,26 @@ dotenv.config();
 // };
 
 export const validateEnv = () => {
+  let mongoUrl = process.env.DATABASE_MONGO_URL_PRISMA;
+
+  if (!mongoUrl) {
+    console.error('DATABASE_MONGO_URL is not set. Please set it in your environment variables.');
+    process.exit(1);
+  }
+
+  if (process.env.NODE_ENV === "production") {
+    mongoUrl = process.env.DATABASE_MONGO_URL_PROD;
+  } else if (process.env.NODE_ENV === "development") {
+    mongoUrl = process.env.DATABASE_MONGO_URL_DEV;
+  }else {
+    console.error('NODE_ENV is not set or is invalid. Please set it to "production" or "development".');
+  }
+  
+  // console.log('MongoDB URL:', mongoUrl);
+
   return {
     port: process.env.PORT,
     env: process.env.NODE_ENV,
-    DATABASE_MONGO_URL: process.env.DATABASE_MONGO_URL,
+    DATABASE_MONGO_URL: mongoUrl,
   };
 };
